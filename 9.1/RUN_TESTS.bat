7a1bb04b4b6f7b6fdb8125c20365426ff2d553c4
indahnurrohmah <indahnurrohmah@mail.ugm.ac.id>
2026-06-03 15:46:10 +0700

    feat: update saving method

--- 9.1/RUN_TESTS.bat
+++ 9.1/RUN_TESTS.bat
@@ -1,11 +1,11 @@
 @echo off
-echo Starting Automation...
 
+echo Starting Automation...
 :: Create report folder if missing
 if not exist "test-output" mkdir test-output
 
-:: Run the JAR with 4GB of RAM allocated
+:: Run the JAR
 :: We explicitly tell it to use 'testing.xml' here!
-java -Xmx4g -jar AutomationApp.jar testing.xml -d test-output
+java -jar AutomationApp.jar testing.xml -d test-output
 
-pause
+pause 
